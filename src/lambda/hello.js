fd51a47a4d27ee74f1b4f79610501436926c06c3
Robert Mill <r.mill@ntlworld.com>
2022-09-30 21:52:43 +0100

    first atempt

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,8 +1,9 @@
 // this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
 export function handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters)
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' }),
-  })
+  // callback(null, {
+  //   statusCode: 200,
+  //   body: JSON.stringify({ msg: 'Hello, World!' }),
+  // })
+  return JSON.stringify({ msg: 'Hello, World!... this a new push' })
 }
